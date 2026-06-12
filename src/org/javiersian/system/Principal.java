14e00e173b634b999018fb55c978227cfbe8d5f9
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-06-12 11:37:07 -0600

    000 Primer commit del proyecto

---
+++ src/org/javiersian/system/Principal.java
@@ -0,0 +1,29 @@
+package org.javiersian.system;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Principal extends Application{
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+       //nodos
+        VBox raiz = new VBox();
+        
+        //escena
+        Scene escena = new Scene(raiz);
+        
+        //cargar escena y mostrar escenario principal
+        escenarioPrincipal.setTitle("Caliculadora de Alvaro");
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+ 
+    }
+}
